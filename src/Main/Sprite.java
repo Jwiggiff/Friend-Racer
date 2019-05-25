3751192d0d81bd6aa043d9f74939f964bc06b525
omster8 <omupatel2003@gmail.com>
2019-05-25 16:55:53 -0400

    Converted Platforms and Obstacles into Sprites

--- src/Main/Sprite.java
+++ src/Main/Sprite.java
@@ -3,18 +3,27 @@ package Main;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
+import javafx.scene.SnapshotParameters;
 
 public class Sprite {
     private Image image;
+    private Image erase;
     private Vector2D pos;
     private Vector2D vel;
-    private int width = 40;
-    private int height = 100;
+    private int width;
+    private int height;
+    ImageView imgView;
 
-    public Sprite() {
+    public Sprite(Image image, int width, Image erase) {
+        this.image = image;
+        this.erase = erase;
         pos = new Vector2D(0,0);
         vel = new Vector2D(0,0);
+        this.width = width;
+        this.height = (int)Math.round(image.getHeight()/(image.getWidth()/width));
+        imgView = new ImageView(image);
     }
 
     public void setPos(int x, int y) {
@@ -38,24 +47,28 @@ public class Sprite {
 
     public Vector2D getPos() {return pos;}
 
-    public void setPos(Vector2D newPos) {pos = newPos;}
-
     public int getHeight() {return height;}
 
     public int getWidth() {return width;}
 
     public void render(GraphicsContext gc)
     {
-        //gc.drawImage(image, pos.x, pos.y);
-        gc.setFill(Color.BLUE);
-        gc.fillRect(pos.x, pos.y, width, height);
+        gc.drawImage(image, pos.x, pos.y, width, height);
+    }
+
+    //TODO: alter the width and height so that when rotated, the image does not shrink
+    public void rotateImage(GraphicsContext gc, double speed) {
+        erase(gc);
+        imgView.setRotate(imgView.getRotate() + speed);
+        SnapshotParameters params = new SnapshotParameters();
+        params.setFill(Color.TRANSPARENT);
+        image = imgView.snapshot(params, null);
+        gc.drawImage(image, pos.x, pos.y, width, height);
     }
 
     public void erase(GraphicsContext gc)
     {
-        //gc.drawImage(image, pos.x, pos.y);
-        gc.setFill(Color.WHITE);
-        gc.fillRect(pos.x, pos.y, width, height);
+        gc.drawImage(erase, pos.x, pos.y, width, height);
     }
 
     private Rectangle2D getBoundary()
