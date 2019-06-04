2ff6f82ddf6056b987e44038ea8368fc4763ab22
omster8 <omupatel2003@gmail.com>
2019-06-03 22:02:17 -0400

    updated the game map

--- src/Main/GameCanvas.java
+++ src/Main/GameCanvas.java
@@ -47,7 +47,7 @@ public class GameCanvas extends Canvas {
             platforms.add(new Sprite(new Image("./platforms/platform_1.PNG"), 75));
             platforms.add(new Sprite(new Image("./platforms/platform.PNG"), 80));
             platforms.add(new Sprite(new Image("./platforms/platform.PNG"), 80));
-            platforms.add(new Sprite(new Image("./platforms/platform.PNG"), 80, 7));
+            platforms.add(new Sprite(new Image("./platforms/platform.PNG"), 80));
             platforms.add(new Sprite(new Image("./platforms/platform.PNG"), 80, 7));
 
             obstacles.add(new Sprite(new Image("./spikes/single_spike.png"), 13));
@@ -59,10 +59,13 @@ public class GameCanvas extends Canvas {
             for (int i = 0; i < 4; i++) {
                 obstacles.add(new Sprite(new Image("./ropes_chains/long_chain_piece.png"), 13));
             }
+            obstacles.add(new Sprite(new Image("./spikes/spikes_1.png"), 50));
+            obstacles.add(new Sprite(new Image("./spikes/single_spike.png"), 10));
+            obstacles.add(new Sprite(new Image("./spikes/single_spike.png"), 10));
 
-            spinningObstacles.add(new SpinningSprite(new Image("./rotating_blades/blade_1.png"), 25, 1));
-            spinningObstacles.add(new SpinningSprite(new Image("./rotating_blades/blade_2.png"), 25, 1));
-            //spinningObstacles.add(new SpinningSprite(new Image("./rotating_blades/blade_3.png"), 25, -1));
+            spinningObstacles.add(new SpinningSprite(new Image("./rotating_blades/blade_1.png"), 24, 1));
+            spinningObstacles.add(new SpinningSprite(new Image("./rotating_blades/blade_2.png"), 24, 1));
+            spinningObstacles.add(new SpinningSprite(new Image("./rotating_blades/blade_3.png"), 24, -1));
             spinningObstacles.add(new SpinningSprite(new Image("./rotating_blades/large_post.png"), 15, 1));
         } catch (Exception e) {
             e.printStackTrace();
@@ -93,38 +96,40 @@ public class GameCanvas extends Canvas {
         platforms.get(41).setPos(390, 420);
         platforms.get(42).setPos(485, 360);
         platforms.get(43).setPos(200, 523);
-
-        platforms.get(44).setPos(820, 525);
-        platforms.get(45).setPos(920, 470);
-        platforms.get(46).setPos(1020, 523);
-        platforms.get(47).setPos(1120, 470);
-        platforms.get(48).setPos(1220, 450);
-        platforms.get(49).setPos(1320, 410);
-        platforms.get(50).setPos(1475, 410);
+        platforms.get(44).setPos(870, 525);
+        platforms.get(45).setPos(970, 470);
+        platforms.get(46).setPos(1070, 523);
+        platforms.get(47).setPos(1210, 470);
+        platforms.get(48).setPos(1310, 450);
+        platforms.get(49).setPos(1420, 410);
+        platforms.get(50).setPos(1595, 410);
 
         obstacles.get(0).setPos(362, 460);
         obstacles.get(1).setPos(457, 400);
         obstacles.get(2).setPos(552, 340);
         obstacles.get(3).setPos(262, 505);
-        obstacles.get(4).setPos(1475, 408 + platforms.get(50).getHeight());
-        obstacles.get(5).setPos(1515, 408 + platforms.get(50).getHeight());
-        obstacles.get(6).setPos(730, 0);
-        obstacles.get(7).setPos(730, obstacles.get(6).getHeight());
-        obstacles.get(8).setPos(730, obstacles.get(6).getHeight()*2);
-        obstacles.get(9).setPos(730, obstacles.get(6).getHeight()*3);
+        obstacles.get(4).setPos(1595, 408 + platforms.get(50).getHeight());
+        obstacles.get(5).setPos(1635, 408 + platforms.get(50).getHeight());
+        obstacles.get(6).setPos(720, 0);
+        obstacles.get(7).setPos(720, obstacles.get(6).getHeight());
+        obstacles.get(8).setPos(720, obstacles.get(6).getHeight()*2);
+        obstacles.get(9).setPos(720, obstacles.get(6).getHeight()*3);
+        obstacles.get(10).setPos(1170, 520);
+        obstacles.get(11).setPos(940, 510);
+        obstacles.get(12).setPos(1280, 455);
 
-        spinningObstacles.get(0).setPos(174, 525);
-        spinningObstacles.get(1).setPos(1275, 425);
-        //spinningObstacles.get(2).setPos(1285, 455);
-        spinningObstacles.get(2).setPos(730 + obstacles.get(6).getWidth()/2 - spinningObstacles.get(2).getWidth()/2, obstacles.get(6).getHeight()*4 - spinningObstacles.get(2).getHeight()/2);
+        spinningObstacles.get(0).setPos(175, 526);
+        spinningObstacles.get(1).setPos(1366, 426);
+        spinningObstacles.get(2).setPos(846, 526);
+        spinningObstacles.get(3).setPos(720 + obstacles.get(6).getWidth()/2 - spinningObstacles.get(3).getWidth()/2, obstacles.get(6).getHeight()*4 - spinningObstacles.get(3).getHeight()/2);
     }
 
     public void rotateSpinningSprites() {
         for (SpinningSprite s : spinningObstacles) {
             if (s.getRotationDirection() == 1) {
-                s.rotateImage(gc, 10);
+                s.rotateImage(gc, 3);
             } else {
-                s.rotateImage(gc, -10);
+                s.rotateImage(gc, -3);
             }
         }
     }
